ed5c108744806a7c45cfb643099f2b597ac2a615
Oliver Ellison, MS SD <89275354+aurelius-in@users.noreply.github.com>
2024-06-26 20:17:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 let video;
-let faceapi;
-let detections = [];
 let usingFrontCamera = true;
 let currentStream;
 let videoOn = false;
@@ -21,72 +19,6 @@ function setup() {
     }
 
     logMessage('ml5 library loaded successfully.');
-
-    // Load the face-api model
-    faceapi = ml5.faceApi(video, { withLandmarks: true, withExpressions: true, withDescriptors: false }, modelReady);
-}
-
-function modelReady() {
-    logMessage('Model Loaded!');
-    faceapi.detect(gotResults);
-}
-
-function gotResults(err, result) {
-    if (err) {
-        logError('Error in gotResults: ' + err);
-        return;
-    }
-
-    detections = result;
-
-    clear();
-    image(video, 0, 0, width, height);
-
-    if (detections && detections.length > 0) {
-        drawBox(detections);
-        drawLandmarks(detections);
-        drawExpressions(detections);
-    }
-
-    faceapi.detect(gotResults);
-}
-
-function drawBox(detections) {
-    for (let i = 0; i < detections.length; i++) {
-        const alignedRect = detections[i].alignedRect;
-        const { _x, _y, _width, _height } = alignedRect._box;
-        stroke(44, 169, 225);
-        strokeWeight(2);
-        noFill();
-        rect(_x, _y, _width, _height);
-    }
-}
-
-function drawLandmarks(detections) {
-    for (let i = 0; i < detections.length; i++) {
-        const parts = detections[i].landmarks.positions;
-        for (let j = 0; j < parts.length; j++) {
-            const { _x, _y } = parts[j];
-            stroke(0, 255, 0);
-            strokeWeight(2);
-            point(_x, _y);
-        }
-    }
-}
-
-function drawExpressions(detections) {
-    for (let i = 0; i < detections.length; i++) {
-        const { expressions } = detections[i];
-        let x = detections[i].alignedRect._box._x;
-        let y = detections[i].alignedRect._box._y - 10;
-        for (const [expression, value] of Object.entries(expressions)) {
-            textSize(16);
-            noStroke();
-            fill(255, 0, 0);
-            text(`${expression}: ${nf(value * 100, 2, 2)}%`, x, y);
-            y += 18;
-        }
-    }
 }
 
 function switchCamera() {
